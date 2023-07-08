64e2d01a9741c1a5477bdef806d4de61eb96ae89
kfandrew910 <kfandrew910@gmail.com>
2023-07-09 00:44:24 +0200

    explicaciones en ft_alphabet

--- c00_elena/ex01/ft_print_alphabet.c
+++ c00_elena/ex01/ft_print_alphabet.c
@@ -9,8 +9,10 @@ void ft_print_alphabet(void)
 	while( primeraLetra <= ultimaLetra)
 	{
 		write(1, &primeraLetra, 1);
-		primeraLetra++;
+		primeraLetra++; // --> primeraLetra = primeraLetra + 1;
 	}
 
 	//write(1,"abcdefghijklmnñopqrstuvwxyz", 28);
+	// string "abc" --> char[3]: ['a']['b']['c']
+	// 			       0    1    2
 }
