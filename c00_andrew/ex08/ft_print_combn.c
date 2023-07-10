436994a77da56a327253150be67bcaeb2e3697e4
kfandrew910 <kfandrew910@gmail.com>
2023-07-10 07:38:38 +0200

    ex08 terminado (andrew)

--- c00_andrew/ex08/ft_print_combn.c
+++ c00_andrew/ex08/ft_print_combn.c
@@ -11,25 +11,37 @@ void ft_print_combn(int n)
 
 	digits[0] = '0';
 	i = 0;
-	while(digits[i] <= 57-n+1)
+	
+	while(digits[0] <= 57-n+1)
 	{
 		if(n > 1)
-			digits[i+1] = digits[i]+1;
-		j=0;
-		while(j <= n)
 		{
-			write(1, &digits[j], 1);
-			if(digits[j] != 57-n+1)
-				write(1, ", ", 2);
-			j++;
+			j=0;
+			while(j < n-1)
+			{
+				digits[j+1] = digits[j]+1;
+				j++;
+			}
 		}
+		while(digits[n-1]<='9')
+		{
+			j=0;
+			while(j <= n-1){	
+				write(1, &digits[j], 1);
+				j++;
+			}
+			if(digits[0] != 57-n+1)
+				write(1, ", ", 2);
+			digits[n-1]++;
+		}
+		digits[0]++;
 		i++;
 	}
 }
 
 int main(void)
 {
-	ft_print_combn(2);
+	ft_print_combn(1);
 	write(1, "\n", 1);
 	return 0;
 }
