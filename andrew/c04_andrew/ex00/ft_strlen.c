552e96ab15f4bf44ea8abaab7893080f4377ba71
kfandrew910 <kfandrew910@gmail.com>
2023-07-18 12:38:01 +0200

    c04 falta terminar (andrew)

---
+++ andrew/c04_andrew/ex00/ft_strlen.c
@@ -0,0 +1,10 @@
+int	ft_strlen(char *str)
+{
+	int	length;
+
+	length = 0;
+	while (str[length] != '\0')
+		length++;
+
+	return length;
+}
---
+++ andrew/c04_andrew/ex02/ft_putnbr.c
@@ -0,0 +1,74 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int ft_pow(int n, int elevated)
+{
+	int el_n;
+	int i;
+
+	el_n = 1;
+	i = 0;
+
+	while(i < elevated)
+	{
+		el_n*=n;
+		i++;
+	}
+
+	return el_n;
+}
+
+int ft_int_length(int n)
+{
+	int length;
+
+	length = 1;
+	while( n > 9 || n < -9 )
+	{
+		n/=10;
+		length++;
+	}
+
+	return length;
+}
+
+int	check_num(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return (0);
+	}
+	return (1);
+}
+
+void ft_putnbr(int nb)
+{
+	int printable;
+	int remover;
+	int size;
+	int i;
+
+	if (check_num(nb) == 0)
+		return ;
+	printable = nb;
+	remover = 0;
+	size = ft_int_length(nb);
+	i = 0;
+	if(nb < 0){
+		ft_putchar('-');
+		nb = nb * (-1);
+	}
+	while(i < size)
+	{
+		printable = nb - remover;
+		printable = printable / ft_pow(10, size-i-1);
+		remover = remover + printable * ft_pow(10, size-i-1);
+		ft_putchar(printable+48);
+		i++;
+	}
+}
