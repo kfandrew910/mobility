f7d6179f6a6bdf4af1291b83a30c73f4753ff053
kfandrew910 <kfandrew910@gmail.com>
2023-07-20 23:02:59 +0200

    hecho el ex03 C03 elena

--- elena/proyectosC/proyectoC03/ex00/ft_strcmp.c
+++ elena/proyectosC/proyectoC03/ex00/ft_strcmp.c
@@ -18,7 +18,6 @@ int	ft_strcmp(char *s1, char *s2)
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
 		if (s1[i] != s2[i])
-			(s1[i] == s2[i])
 			return (s1[i] - s2[i]);
 		i++;
 	}
--- elena/proyectosC/proyectoC03/ex01/ft_strncmp.c
+++ elena/proyectosC/proyectoC03/ex01/ft_strncmp.c
@@ -5,9 +5,23 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: amoisac <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/20 12:55:23 by amoisac           #+#    #+#             */
-/*   Updated: 2023/07/20 12:55:35 by amoisac          ###   ########.fr       */
+/*   Created: 2023/07/20 12:11:29 by amoisac           #+#    #+#             */
+/*   Updated: 2023/07/20 12:54:15 by amoisac          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_strncmp(char *s1, char *s2, unsigned	int n)
+{
+	unsigned int	i;
 
+	if (n < 1)
+		return (0);
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
