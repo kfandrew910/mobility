2c78bfd6a67224631da0d5c4e46291a0ddf25934
kfandrew910 <128989944+kfandrew910@users.noreply.github.com>
2023-07-23 17:44:06 +0200

    Add files via upload

--- andrew/c02_andrew/ex01/ft_strncpy.c
+++ andrew/c02_andrew/ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: akovacs- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 13:25:42 by akovacs-          #+#    #+#             */
-/*   Updated: 2023/07/17 14:09:49 by akovacs-         ###   ########.fr       */
+/*   Updated: 2023/07/23 15:56:12 by akovacs-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,18 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	unsigned int	i;
 
 	i = 0;
-	while (i < n)
+	while (src[i] != 0 && i < n)
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	while (dest[i] != '\0')
+	while (i < n)
+	{
+		dest[i] = 0;
 		i++;
-	dest[i] = '\0';
+	}
+	while (dest[i] != 0)
+		i++;
+	dest[i] = 0;
 	return (dest);
 }
